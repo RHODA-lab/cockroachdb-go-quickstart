7e02ae93940ea87c4fefa855c50153cda9f79966
Marco Yeung <myeung@redhat.com>
2022-07-26 12:46:48 -0400

    update fruit schema

--- pkg/database/migrations/20220312095619_add_fruit_table.sql
+++ pkg/database/migrations/20220312095619_add_fruit_table.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 
-CREATE TABLE IF NOT EXISTS Fruit(id varchar(100) PRIMARY KEY , name varchar(100), quantity varchar(11) null, description varchar(200));
+CREATE TABLE IF NOT EXISTS Fruit(id varchar(100) PRIMARY KEY , name varchar(100), quantity varchar(11) null, description varchar(200) null);
 
 INSERT INTO Fruit (id, name, quantity, description) VALUES ('d37f4fae-b572-47b3-93e0-17daf798f9d5', 'Banana', '0', 'Good for health') ON CONFLICT (ID) DO NOTHING;
 INSERT INTO Fruit (id, name, quantity, description) VALUES ('51661376-0a07-449b-a3bd-9db79cd4ead4', 'Apple', '0', 'Keeps the doctor away') ON CONFLICT (ID) DO NOTHING;
